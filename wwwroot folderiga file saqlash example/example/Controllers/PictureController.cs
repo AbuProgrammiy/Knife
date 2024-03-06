da8500b3c19cac36c9994966ca74b93a47ba2805
Abu_Programmiy <abdukholiq23@gmail.com>
2024-03-06 22:09:48 +0500

    imkoniyatlar kengaymoqda

---
+++ wwwroot folderiga file saqlash example/example/Controllers/PictureController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;         // ControllerBase |ishlashi uchun
+
+namespace example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PictureController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;       // IWebHostEnvironment faqat Api ichida ishlaydi!
+
+        public PictureController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        [Route("get")]
+        public IActionResult GetPicture(IFormFile picture)
+        {
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "images", picture.FileName);
+            using (FileStream strem =new FileStream(path,FileMode.Create))
+                picture.CopyTo(strem);
+
+            var fileBytes = System.IO.File.ReadAllBytes(path);
+            return File(fileBytes, "image/jpeg"); 
+        }
+    }
+}
